fbf64194213412a830dacd8c90a72a3a0661c08c
Yuan <ziyaajiang@gmail.com>
2017-04-21 16:18:55 -0500

    done

--- multiply.py
+++ multiply.py
@@ -395,6 +395,10 @@ def main():
                   , R_D1, R_E1, R_D, R_E, P_B, R_F0, P, R_L, R_I, R_B
                   , dict=True)
 
+    ss5_2 = solve((meq1, feq1, eq1, eq2, eq3, eq4, beq11, beq2, beq3, beq4)
+                  , R_D1, R_E1, R_D, R_E, P_B, R_F0, P, R_L, R_I, R_B, I
+                  , dict=True)
+
     ss6 = solve((feq1, eq1, eq2, eq3, eq4, beq1, beq2, beq3, beq4, beq5)
                 , R_D1, R_E1, R_D, R_E, P_B, R_F0, P, R_L, R_I
                 , dict=True)
